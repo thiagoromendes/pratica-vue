9bb04e243064230b223fb60182b8d6876b21a0b4
Thiago Mendes <thiagoromendes@gmail.com>
2020-11-25 01:10:15 -0300

    Introdução aos componentes Vue

---
+++ src/components/Contador.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="contador">
+      <span>{{contador}}</span>
+      <button @click="add">+</button>
+      <button @click="sub">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            contador: 0
+        }
+    },
+    methods: {
+        add(){
+            this.contador++
+        },
+        sub(){
+            this.contador--
+        }
+    }
+}
+</script>
+
+<style>
+    .contador {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .contador span {
+        font-size: 30px;
+        padding: 5px 10px;
+    }
+
+    .contador button {
+        height: 30px;
+        width: 30px;
+        border-radius: 50%;
+        background: #000;
+        color: #fff;
+        font-weight: bold;
+        font-size: 15px;
+        margin-left: 10px;
+        outline: none;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Contadores from './components/Contadores'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+Vue.component('app-contadores', Contadores)
 
 new Vue({
   router,
